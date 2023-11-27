ced1bbf72c219487b894444185d77acffe53437f
tanyashevlyakova <shevlyakovatanya83@gmail.com>
2023-11-27 23:36:53 +0200

    REQ-01: Base authentication

--- src/main/java/com/magicvet/Main.java
+++ src/main/java/com/magicvet/Main.java
@@ -24,7 +24,7 @@ public class Main {
             }
         }
 
-        System.out.println(accepted ? "Welcome to the Magic Vet!" : "Application has been blocked.");
+        System.out.println(accepted ? "Welcome to the Magic Vet!": "Application has been blocked.");
     }
 
 }
